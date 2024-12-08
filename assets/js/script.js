7cd330a8d62c9e7ae1b903268a327c2fab466634
Shashwat Deo <121250888+Shashwatdeo@users.noreply.github.com>
2024-12-08 23:46:40 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,11 +59,11 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | CodeWithTanveer";
+            document.title = "Portfolio | ShashwatDeo";
             $("#favicon").attr("href", "assets/images/favicon.jpg");
         }
         else {
-            document.title = "Portfolio | Tanveer Ahmad";
+            document.title = "Portfolio | Shashwat Deo";
             $("#favicon").attr("href", "assets/images/favicon.jpg");
         }
     });
